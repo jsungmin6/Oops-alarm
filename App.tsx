2a172f8d4836b86b0c392360963c3c0b971d55e3
Taemin Kim <59005171+jsungmin6@users.noreply.github.com>
2025-08-04 06:43:57 +0900

    feat: add swipe to delete alarms

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { RootStackParamList } from './types/navigation'
 import HomeScreen from './screens/HomeScreen'
 import CreateAlarmScreen from './screens/CreateAlarmScreen'
@@ -10,13 +11,15 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="CreateAlarm" component={CreateAlarmScreen} />
-          <Stack.Screen name="EditAlarm" component={EditAlarmScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="CreateAlarm" component={CreateAlarmScreen} />
+            <Stack.Screen name="EditAlarm" component={EditAlarmScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GestureHandlerRootView>
   );
 }
 
