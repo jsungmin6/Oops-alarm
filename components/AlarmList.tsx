03b0a4c3961859cea61320d59ab248a65aee2953
Taemin Kim <59005171+jsungmin6@users.noreply.github.com>
2025-08-09 17:45:44 +0900

    Add swipeable alarm list with delete action

---
+++ components/AlarmList.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { FlatList, StyleSheet } from 'react-native'
+import AlarmRow from './AlarmRow'
+import { Alarm } from '../types/Alarm'
+
+type Props = {
+    alarms: Alarm[]
+    deleteAlarm: (id: string) => void
+    updateAlarmDate: (id: string) => void
+    onEdit: (id: string) => void
+}
+
+const AlarmList = ({ alarms, deleteAlarm, updateAlarmDate, onEdit }: Props) => (
+    <FlatList
+        data={alarms}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+            <AlarmRow
+                alarm={item}
+                deleteAlarm={deleteAlarm}
+                updateAlarmDate={updateAlarmDate}
+                onEdit={onEdit}
+            />
+        )}
+        contentContainerStyle={styles.container}
+    />
+)
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+        paddingBottom: 80,
+    },
+})
+
+export default AlarmList
+
