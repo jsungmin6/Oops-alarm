ec488420e08ab1f476ce419a83ebb261fcbb48fe
taeminkim <jsungmin506@gmail.com>
2025-08-14 00:56:04 +0900

    날짜포맷변경

--- components/DatePickerField.tsx
+++ components/DatePickerField.tsx
@@ -16,7 +16,7 @@ const formatKoreanDate = (date: Date) => {
     const year = date.getFullYear()
     const month = String(date.getMonth() + 1).padStart(2, '0')
     const day = String(date.getDate()).padStart(2, '0')
-    return `${year}년 ${month}월 ${day}일`
+    return `${year}/${month}/${day}`
 }
 
 interface Props {
